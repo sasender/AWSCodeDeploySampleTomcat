ce3e1b481e0d8fbf4b563c2a5d4fe7410ade4c39
sasender <78895362+sasender@users.noreply.github.com>
2021-12-27 15:48:49 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
 aws s3 cp s3://s3://mavendemo/maven-demo/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
-sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
+sudo mv /tmp/LoginWebApp-1.war /root/apache-tomcat/webapps/LoginWebApp-1.war
 sudo service tomcat restart
